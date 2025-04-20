eef544b69468e6b47176aafc2597cdd748b70fa5
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2025-04-20 16:04:43 +0200

    Stop clicking when the cutscene is playing

--- src/components/Cutscene/Cutscene.tsx
+++ src/components/Cutscene/Cutscene.tsx
@@ -1,6 +1,6 @@
 export function Cutscene() {
   return (
-    <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+    <div className="absolute inset-0 flex items-center justify-center">
       <video className="w-full h-full" width="720" autoPlay>
         <source src="/videos/1in100.mov" type="video/mp4" />
       </video>
