2f014f2833b2e55d5ac4c0362a18b34402957538
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2025-05-09 19:51:00 +0200

    Improves video loading and playback
    Adds preload and muted attributes to the video element to improve video loading and playback experience, particularly for autoplay scenarios.

--- src/components/Cutscene/Cutscene.tsx
+++ src/components/Cutscene/Cutscene.tsx
@@ -1,7 +1,7 @@
 export function Cutscene() {
   return (
     <div className="absolute inset-0 flex items-center justify-center">
-      <video className="w-full h-full" width="720" autoPlay>
+      <video className="w-full h-full" width="720" autoPlay preload="auto" muted>
         <source src="/videos/1in100.mov" type="video/mp4" />
       </video>
     </div>
