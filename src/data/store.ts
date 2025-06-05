026160806b55656a13f0e2c6bc763a05e1e3d94c
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2025-06-05 22:19:13 +0200

    Adds more store items
    Adds new temporary and permanent luck modifiers to the store.
    
    This allows players to purchase a wider variety of luck boosts, both for a limited time and permanently.

--- src/data/store.ts
+++ src/data/store.ts
@@ -23,11 +23,42 @@ export const storeItems: StoreItemType[] = [
     type: "temporaryModifier",
   },
   {
+    cost: 5000,
+    label: "x5 luck",
+    luckModifier: 5,
+    duration: 105,
+    type: "temporaryModifier",
+  },
+  {
     cost: 10000,
     label: "x10 luck",
     luckModifier: 10,
     duration: 120,
     type: "temporaryModifier",
+  },
+  {
+    cost: 5000,
+    label: "x0.5 luck",
+    luckModifier: 0.5,
+    type: "permanentModifier",
+  },
+  {
+    cost: 10000,
+    label: "x1 luck",
+    luckModifier: 1,
+    type: "permanentModifier",
+  },
+  {
+    cost: 20000,
+    label: "x2 luck",
+    luckModifier: 2,
+    type: "permanentModifier",
+  },
+  {
+    cost: 50000,
+    label: "x5 luck",
+    luckModifier: 5,
+    type: "permanentModifier",
   },
   {
     cost: 100000,
